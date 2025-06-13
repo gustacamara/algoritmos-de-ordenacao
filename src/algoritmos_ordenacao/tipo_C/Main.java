d16d892d2d3a3cf2cdd36f6799adecae95e5a49c
Reshi <duuhmorciani@gmail.com>
2025-06-13 20:29:34 -0300

    Implementa a o metódo de ordenação TimSort

--- src/algoritmos_ordenacao/tipo_C/Main.java
+++ src/algoritmos_ordenacao/tipo_C/Main.java
@@ -1,4 +1,42 @@
 package algoritmos_ordenacao.tipo_C;
 
+import java.util.Random;
+
 public class Main {
+    public static void main(String[] args) {
+//        int[] dados = new int[100];
+//        int tamanho = 50;
+//
+//        COCKTAIL
+//        Random rand = new Random();
+//        long inicio = System.currentTimeMillis();
+//        for (int i = 0; i < tamanho; i++) {
+//            dados[i] = rand.nextInt(1000);
+//        }
+//        long fim =  (System.currentTimeMillis() - inicio);
+//        System.out.println("O tempo foi de: " + fim);
+//
+//        CockTail cockTailSort = new CockTail(dados,tamanho);
+//        cockTailSort.cockTail();
+//        cockTailSort.imprimir();
+//
+
+//        TIMESORT
+//            Random rand = new Random();
+//            long inicio = System.currentTimeMillis();
+//            for (int i = 0; i < tamanho; i++) {
+//                int numeros = rand.nextInt(10000);
+//                dados[i] = numeros;
+//                System.out.println(numeros);
+//            }
+//            long fim =  (System.currentTimeMillis() - inicio);
+//            System.out.println("O tempo foi de: " + fim);
+//
+//            TimSort timSort = new TimSort(dados, tamanho);
+//
+//            timSort.timSort();
+//
+//            timSort.imprimir(dados, tamanho);
+    }
+
 }
