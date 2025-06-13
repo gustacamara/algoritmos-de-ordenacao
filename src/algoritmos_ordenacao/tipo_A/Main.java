251b57df7708c710246603ab11df38125115efe4
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2025-06-13 17:11:48 -0300

    Adiciona combsort

--- src/algoritmos_ordenacao/tipo_A/Main.java
+++ src/algoritmos_ordenacao/tipo_A/Main.java
@@ -1,4 +1,17 @@
 package algoritmos_ordenacao.tipo_A;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] lista = {10, 3, 39, 8, 23, 19, 20};
+
+        CombSort ordenador = new CombSort(7, lista);
+
+        System.out.println("Lista original:");
+        ordenador.imprimir();
+
+        ordenador.combsort(lista);
+
+        System.out.println("Lista ordenada pelo CombSort:");
+        ordenador.imprimir();
+    }
 }
